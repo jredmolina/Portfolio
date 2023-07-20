14e5e6ffd3afd041788707953118b2e5ec6a7316
jredmolina <jared.a.molina01@gmail.com>
2023-07-20 04:42:37 -0700

    Hero text added back

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,9 @@ const Hero = forwardRef((props, ref) => {
             whileInView={{ scale: 1 }}
             initial={{ scale: 0 }}
             className="text-[100px] max-lg:text-[40px] leading-none"
-          ></motion.h1>
+          >
+            Hi, I'm <strong>Jared</strong>
+          </motion.h1>
           <div className="flex items-center gap-[10px]">
             <img className="h-[5px]" src="./img/line.png"></img>
           </div>
